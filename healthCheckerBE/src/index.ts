ca71ece5754b86eae38bb035a56eb7c5fe6a0e6f
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-12-18 19:01:32 +0530

    cors fix

--- healthCheckerBE/src/index.ts
+++ healthCheckerBE/src/index.ts
@@ -8,7 +8,11 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const urlSchema = z.string().url();
 const urlsSchema = z.array(urlSchema);
--- primaryBE/src/index.ts
+++ primaryBE/src/index.ts
@@ -26,7 +26,11 @@ client.on('error', (err) => console.log('Redis Client Error', err));
 })();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 // add all routes
 app.use('/api', router)
